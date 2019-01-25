c4d3c1ce770c1130ef6808d8ca2e7d30aaadbaa8
ml4713 <ml4713@nyu.edu>
2019-01-25 07:23:34 -0500

    rename folder

--- sagemaker/pipeline.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import random
-
-
-def get_data(k=100):
-    return pd.DataFrame({'x': np.random.uniform(0, 1, size=k),
-                         'y': random.choices(['class1', 'class2'], k=k)})
